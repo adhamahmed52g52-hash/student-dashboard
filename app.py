d97fdcb5c69a4290fa14e1b507234f4347c98f3e
adhamahmed52g52-hash <adhamahmed52g52@gmail.com>
2026-09-08 19:47:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df=pd.read_excel('Attachments/students_data.xlsx')
+df = pd.read_excel('students_data.xlsx')
 print(df)
 # print(df.at[0,'Age'])
 # print(df.index)
